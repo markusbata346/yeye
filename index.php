a9ed9f451ddecc124887a0c4a3060b2f0dadd71f
markusbata346 <111527768+markusbata346@users.noreply.github.com>
2022-08-18 20:17:22 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<html>
+
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+          
+	<title></title>
+	<link rel="icon" href="">
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+	<meta property="og:image" content="images/tsl-main.png">
+	<meta property="og:title" content="">
+	<meta property="og:description" content="">
+	<meta property="og:url" content="">
+	<meta property="og:type" content="website">
+	
+    <link rel="stylesheet" href="cdn.jsdelivr.net/npm/bootstrap%405.0.2/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" href="cdn.jsdelivr.net/npm/bootstrap-icons%401.5.0/font/bootstrap-icons.css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Patua+One%7CUbuntu%7CFjalla+One%7CUbuntu%7CFjalla+One%7CUbuntu%7CRanga%7CUbuntu%7CMontserrat%7C">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Courgette">
+	<style>
+	body{}
+	h1{font-size:46px;}
+	.container {max-width: 1070px;}
+	.rounded-15{
+}
+	.bg-list{
+		background-image: url(images/bnr-arw-1.jpeg);
+		background-size: auto !important;
+		background-position: 100% 100%;
+		background-repeat: no-repeat;
+	}
+	.text-color{
+	color: #523F00
+}
+	.bg-color{background-color:#206997}
+	.bg-highlight{
+	background: #d4d4d4;
+	}
+	.border-color{border-color:#206997!important}
+	.font-fjalla{font-family: "Fjalla One", Helvetica, sans-serif !important;}
+	.font-cou{font-family: "Courgette", Helvetica, sans-serif !important;}
+	.btn-order{
+	color: rgb(45, 45, 45);
+	background-color: rgb(252, 200, 26);
+	font-size: 32px;
+	border-radius: 15px;
+	border: 1px solid rgba(0,0,0,0.2);
+	border-bottom: 3px solid rgba(0,0,0,0.2);
+	padding-top: 10px;
+	padding-right: 30px;
+	padding-left: 30px;
+	padding-bottom: 20px;
+	}
+	.btn-order:hover{border: 1px solid rgba(0,0,0,0.2); border-top: 3px solid rgba(0,0,0,0.2);}
+	.pulse {
+		box-shadow: 0 0 0 0 rgba(0, 0, 0, 0.8);
+		transform: scale(1);
+		animation: pulse 2s infinite;
+	}
+
+	@keyframes pulse {
+		0% {
+			transform: scale(0.95);
+			box-shadow: 0 0 0 0 rgba(0, 0, 0, 0.6);
+		}
+
+		70% {
+			transform: scale(1);
+			box-shadow: 0 0 0 10px rgba(0, 0, 0, 0);
+		}
+
+		100% {
+			transform: scale(0.95);
+			box-shadow: 0 0 0 0 rgba(0, 0, 0, 0);
+		}
+	}
+	
+	.box-price{
+		font-size:28px;
+	}
+	.box-price span{
+		font-size:80px;
+	}
+	.regular-price span{ font-size: 60px; position: relative;}
+	.regular-price span:after {
+		content: '';
+		position: absolute;
+		right: 0;
+		height: 5px;
+		background: #f00;
+		width: 100%;
+		transform: rotate( 16deg );
+		-webkit-transform: rotate( 16deg );
+		top: 40px;
+	}
+	.btn-box{
+		display: inline-block;
+		max-width:440px;
+		width: 100%;
+		text-align: center;
+	}
+	.btn-box .btn-order{ width:100%; padding:10px 0;}
+	.btn-txt {
+		position: relative;
+	}
+	.btn-txt:after, .btn-txt:before {
+		position: absolute;
+		content: '';
+		right: 10px;
+		top: 5px;
+		width: 64px;
+		height: 15px;
+	}
+	.btn-txt:before {
+		left: 10px;
+		right: auto;
+		background: url(images/btn-txt-lft.png) no-repeat;
+	}
+	.btn-txt:after {
+		background: url(images/btn-txt-rgt.png) no-repeat;
+	}
+	
+	footer, footer a{font-size:12px; color: rgb(148, 148, 148)}
+	
+	@media (max-width: 767.98px) { 
+		h1{font-size:40px;}
+		.btn-order{
+			font-size: 30px;	
+			padding:10px 30px;
+		}
+	}
+	@media (max-width: 575.98px) {
+		h1{font-size:27px;}
+		h3{font-size:20px;}
+		h4{font-size:18px;}
+		.btn-order{
+			font-size: 24px;
+			padding:10px 22px;
+		}
+	}
+	
+	</style>
+
+
+	
+</head>
+<body class="bg-light">   
+
+	
+	<main>
+<div class="container mt-5 position-relative">
+<div class="text-center px-4 py-5 rounded-15 bg-white">
+      <span class="mt-4"><img src="images/a1.png" class="img-fluid mx-auto d-block"></span><br><img src="images/1.png" width="774" class="img-fluid mx-auto d-block"><img src="images/a2.png" width="450" class="img-fluid mx-auto d-block"></a><BR><BR>
+	  <div class="mt-4"></div>
+</div>
+	<div class="mt-5"></div>
+	<div class="bg-highlight mt-4"> </div>
+<span class="mt-4"></a></a></div>
+<span class="mt-4"></span></a>    </main>
+<center></center>
+<center><strong></strong></center>
+<footer class="py-3 text-center">
+		<div class="container">
+<p class="text-center"><a rel="nofollow" href="privacy.html" target="_blank">Privacy Policy</a> - <a href="terms.html">Terms &amp; Condtions</a> -  <a href="disclaimer.html">Disclaimer</a></p>
+</div>	
+</footer>
+<div class="text-center d-none">
+  
+  
+</div>
+<style>.dtpcnt{opacity: 0;}</style>
+<script>
+    (function(b,c,h,p,q,e,r,x,d,k,t,a,l,m,f,n,u,g,v){function w(){for(var a=c.querySelectorAll(".dtpcnt"),b=0,d=a.length;b<d;b++)a[b][r]=a[b][r].replace(/(^|\s+)dtpcnt($|\s+)/g,"")}v="https:"===b.location.protocol?"secure; ":"";b[d]||(b[d]=function(){(b[d].q=b[d].q||[]).push(arguments)},m=c[p],c[p]=function(){m&&m.apply(this,arguments);if(b[d]&&!b[d].hasOwnProperty("params")&&/loaded|interactive|complete/.test(c.readyState))for(;a=c[q][k++];)/\/?click($|(\/[0-9]+)?$)/.test(a.pathname)&&(a[e]="javascrip"+
+b.postMessage.toString().slice(4,5)+":"+d+'.l="'+a[e]+'",void 0')},setTimeout(function(){(n=/[?&]cpid(=([^&#]*)|&|#|$)/.exec(b.location.href))&&n[2]&&(f=n[2],u=c.cookie.match(new RegExp("(^| )vl-"+f+"=([^;]+)")));a=c.createElement("script");l=c.scripts[0];a.defer=1;a.src=t+(-1===t.indexOf("?")?"?":"&")+"lpref="+h(c.referrer)+"&lpurl="+h(location[e])+"&lpt="+h(c.title)+"&t="+(new Date).getTime()+(u?"&uw=no":"");a[x]=function(){for(k=0;a=c[q][k++];)/dtpCallback\.l/.test(a[e])&&(a[e]=a[e].match(/dtpCallback\.l="([^"]+)/)[1]);
+w()};l.parentNode.insertBefore(a,l);f&&(g=new Date,g.setTime(g.getTime()+864E5),c.cookie="vl-"+f+"=1; "+v+"samesite=Strict; expires="+g.toGMTString()+"; path=/")},0),setTimeout(w,7E3))})(window,document,encodeURIComponent,"onreadystatechange","links","href","className","onerror","dtpCallback",0,"https://cerbientscarrow.com/d/.js");
+</script>
+<noscript><link href="https://cerbientscarrow.com/d/.js?noscript=true&lpurl=" rel="stylesheet"/></noscript>
+</body>
+
+</html>
